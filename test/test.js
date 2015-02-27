e9b5d9563bf62afb11e49d819208aff716b20f01
Claudio Amoedo <claudioamoedo@gmail.com>
2015-02-27 15:35:51 -0300

    Mocha setup

--- test/test.js
+++ test/test.js
@@ -1,9 +1,13 @@
-var should = require('should');
+'use strict';
+
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
       [1,2,3].indexOf(5).should.be.equal(-1);
       [1,2,3].indexOf(0).should.be.equal(-1);
+      // must call done() so that mocha know that we are... done.
+      // Useful for async tests.
+      done();      
     });
   });
 });
